c4bba9f454149b434b468c2f564c5a97a0e763c3
James Rippon <jrippon@zaizi.com>
2022-02-07 17:52:13 +0000

    updated to work as a module

--- iamuser/main.tf
+++ iamuser/main.tf
@@ -1,12 +1,10 @@
 resource "aws_iam_user" "moduleuser" {
-  provider = aws.users
   for_each = toset(var.usernames)
   name = each.key
   path = "/"
 }
 
 resource "aws_iam_user_group_membership" "allusers" {
-  provider = aws.users
   for_each = aws_iam_user.moduleuser
   user = each.value.name
   groups = [ 
@@ -15,12 +13,10 @@ resource "aws_iam_user_group_membership" "allusers" {
 }
 
 resource "aws_iam_group" "all_users" {
-  provider = aws.users
 	name = "all_users"
 }
 
 resource "aws_iam_group_policy" "manage_own_creds" {
-  provider = aws.users
 	name = "manage_own_creds"
   group = aws_iam_group.all_users.name
 
