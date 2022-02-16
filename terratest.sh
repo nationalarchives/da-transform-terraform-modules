73797faf2949f723c3878907ca463c61e3d5ee7b
rahmedzaizi <rahmed@zaizi.com>
2022-02-16 02:19:57 +0000

    terratest script added to test remote modules

--- terratest.sh
+++ terratest.sh
@@ -6,8 +6,8 @@ set -e
 go mod init test
 go mod tidy
 
-for dir in $(find ../da-transform-terraform-modules -maxdepth 1 -mindepth 1 -type d ); do
-   if [[ -f "../da-transform-terraform-modules/${dir}/test/test.sh" ]]; then
-        ../da-transform-terraform-modules/${dir}/test/test.sh
+for dir in $(find ./.git/da-transform-terraform-modules -maxdepth 1 -mindepth 1 -type d ); do
+   if [[ -f "./.git/da-transform-terraform-modules/${dir}/test/test.sh" ]]; then
+        ./.git/da-transform-terraform-modules/${dir}/test/test.sh
    fi
 done
