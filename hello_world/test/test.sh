3c86226b593247b6a100ff438b876c10a60f4cfd
rahmedzaizi <rahmed@zaizi.com>
2022-02-16 11:20:57 +0000

    modified script to create terratest logs

--- hello_world/test/test.sh
+++ hello_world/test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-
+echo "Running terratest for hellow_world module"
 go get "github.com/gruntwork-io/terratest/modules/terraform" \
    "github.com/stretchr/testify/assert" \
    "strings" \
@@ -10,4 +10,10 @@ go get "github.com/gruntwork-io/terratest/modules/terraform" \
    "github.com/gruntwork-io/terratest/modules/random" \
    "github.com/gruntwork-io/terratest/modules/aws"
 
-go test ../da-transform-terraform-modules/hello_world/test/hello_world_test.go -v
+mkdir -p ../da-transform-terraform-modules/test/reports/hellow_world
+
+go test ../da-transform-terraform-modules/hello_world/test/hello_world_test.go -v | tee ../da-transform-terraform-modules/test/reports/hello_world_test_output.log
+
+echo "Creating terratest logs"
+
+terratest_log_parser -testlog ../da-transform-terraform-modules/test/reports/hello_world_test_output.log -outputdir ../da-transform-terraform-modules/test/reports/hellow_world
