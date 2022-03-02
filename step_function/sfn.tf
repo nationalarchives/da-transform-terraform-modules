b3906125c8023c689e020afb2c1fa8277cd5b008
rahmedzaizi <rahmed@zaizi.com>
2022-03-02 01:55:36 +0000

    added steps to state machine

--- step_function/sfn.tf
+++ step_function/sfn.tf
@@ -1,7 +1,9 @@
 resource "aws_sfn_state_machine" "tdr_state_machine" {
   name = "${var.env}-retrive-bagit-machine"
   role_arn = aws_iam_role.tdr_state_machine_role.arn
-  definition = file("${path.module}/templates/step-function-definition.json.tftpl")
+  definition = templatefile("${path.module}/templates/step-function-definition.json.tftpl", {
+    bagit_checksum_lambda = aws_lambda_function.retrieve_bagit_function.arn
+  })
   logging_configuration {
      log_destination = "${aws_cloudwatch_log_group.tdr_state_machine_logs.arn}:*"
     include_execution_data = true
