fde97b4404b99b73412bedb66312ddd6f0db1eeb
rahmedzaizi <rahmed@zaizi.com>
2022-03-03 03:45:46 +0000

    Simmple sfn test

--- step_function/sfn.tf
+++ step_function/sfn.tf
@@ -1,23 +1,10 @@
 resource "aws_sfn_state_machine" "tdr_state_machine" {
   name     = "${var.env}-te-state-machine"
   role_arn = aws_iam_role.tdr_state_machine_role.arn
-  definition = <<EOF
-  {
-  "Comment": "A Hello World example of the Amazon States Language using Pass states",
-  "StartAt": "Hello",
-  "States": {
-    "Hello": {
-      "Type": "Pass",
-      "Next": "World"
-    },
-    "World": {
-      "Type": "Pass",
-      "Result": "World",
-      "End": true
-    }
-  }
-}
-  EOF
+  definition = templatefile("${path.module}/templates/step-function-definition.json.tftpl", {
+    bagit_checksum_lambda = aws_lambda_function.retrieve_bagit_function.arn 
+    files_checksum_lambda = aws_lambda_function.bagit_files_checksum_function.arn
+  })
   logging_configuration {
     log_destination        = "${aws_cloudwatch_log_group.tdr_state_machine_logs.arn}:*"
     include_execution_data = true
