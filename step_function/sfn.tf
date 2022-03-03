73a0c917f4e8d74ead12a3ca7e0c13bb47d08188
rahmedzaizi <rahmed@zaizi.com>
2022-03-03 12:17:37 +0000

    solving state machine error

--- step_function/sfn.tf
+++ step_function/sfn.tf
@@ -1,10 +1,7 @@
 resource "aws_sfn_state_machine" "tdr_state_machine" {
   name     = "${var.env}--te-state-machine"
   role_arn = aws_iam_role.tdr_state_machine_role.arn
-  definition = templatefile("${path.module}/templates/step-function-definition.json.tftpl", {
-    bagit_checksum_lambda = aws_lambda_function.retrieve_bagit_function.arn 
-    files_checksum_lambda = aws_lambda_function.bagit_files_checksum_function.arn
-  })
+  definition = file("${path.module}/templates/step-function-definition.json.tftpl")
   logging_configuration {
     log_destination        = "${aws_cloudwatch_log_group.tdr_state_machine_logs.arn}:*"
     include_execution_data = true
