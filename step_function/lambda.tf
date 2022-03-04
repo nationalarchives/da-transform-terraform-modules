286efd05bba93dc09c178ec5a3ac67ea4e0f733f
rahmedzaizi <rahmed@zaizi.com>
2022-03-04 19:26:16 +0000

    step function updated for retry checksum

--- step_function/lambda.tf
+++ step_function/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "retrieve_bagit_function" {
-  image_uri     = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-bagit-checksum-validation:0.0.1"
+  image_uri     = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-bagit-checksum-validation:0.0.2"
   package_type  = "Image"
   function_name = "${var.env}-te-bagit-checksum-validation"
   role          = aws_iam_role.retrieve_bagit_lambda_role.arn
