0ac9b44f6816df5bed49868dd12204ecbfb5d303
rahmedzaizi <rahmed@zaizi.com>
2022-03-04 20:35:57 +0000

    step function updated for retry checksum

--- step_function/variables.tf
+++ step_function/variables.tf
@@ -8,7 +8,7 @@ variable "tdr_sqs_queue_endpoint" {
   type = string
 }
 
-variable "tdr_sqs_queue_arn" {
-  description = "ARN of the TDR SQS Queue for the retry message"
-  type = string
-}
+# variable "tdr_sqs_queue_arn" {
+#   description = "ARN of the TDR SQS Queue for the retry message"
+#   type = string
+# }
