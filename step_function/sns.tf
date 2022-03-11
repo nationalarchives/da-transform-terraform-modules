519a2f0823797a1842edfc96b61db19e7e05f830
rahmedzaizi <rahmed@zaizi.com>
2022-03-11 01:41:21 +0000

    sns for editorial added to state func

--- step_function/sns.tf
+++ step_function/sns.tf
@@ -1,5 +1,9 @@
 resource "aws_sns_topic" "editorial_sns" {
   name = "${var.env}-te-editorial-out"
-  policy = data.aws_iam_policy_document.editorial_sns_topic_policy.json
   kms_master_key_id = "alias/aws/sns"
+}
+
+resource "aws_sns_topic_policy" "editorial_sns_policy" {
+  arn = aws_sns_topic.editorial_sns.arn
+  policy = data.aws_iam_policy_document.editorial_sns_topic_policy.json
 }
