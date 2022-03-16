8f98fc8abf52edd1f3652538bc152c7021199930
rahmedzaizi <rahmed@zaizi.com>
2022-03-16 00:33:30 +0000

    added env variables to editorial_integration_lambda

--- step_function/lambda.tf
+++ step_function/lambda.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "bagit_files_checksum_function" {
 }
 
 resource "aws_lambda_function" "run_judgments_parser" {
-  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-text-parser-step-function:latest"
+  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-text-parser-step-function:0.0.8"
   package_type = "Image"
   function_name = "${var.env}-te-run-judgments-parser"
   role = aws_iam_role.retrieve_bagit_lambda_role.arn
@@ -35,14 +35,14 @@ resource "aws_lambda_function" "run_judgments_parser" {
 }
 
 resource "aws_lambda_function" "editorial_integration" {
-  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-editorial-integration:0.0.2"
+  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-editorial-integration:0.0.6"
   package_type = "Image"
   function_name = "${var.env}-te-editorial-integration"
   role = aws_iam_role.retrieve_bagit_lambda_role.arn
   timeout = 30
   environment {
     variables = {
-      "TE_VERSION_JSON" = "{}"
+      "TE_VERSION_JSON" = {"int-te-version": "0.0.0", "text-parser-version": "v0.0", "lambda-functions-version": [ { "int-te-bagit-checksum-validation": "0.0.0" }, { "int-te-files-checksum-validation": "0.0.0" }, { "int-text-parser-version": "v0.0" } ]}
       "TE_PRESIGNED_URL_EXPIRY" = 300
     }
   }
