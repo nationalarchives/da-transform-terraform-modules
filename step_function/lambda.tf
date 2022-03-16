a9751abc32943f0d9fc645ac251f8ac2dbde8f8a
rahmedzaizi <rahmed@zaizi.com>
2022-03-16 15:22:57 +0000

    variables added to lambda functions

--- step_function/lambda.tf
+++ step_function/lambda.tf
@@ -32,6 +32,12 @@ resource "aws_lambda_function" "run_judgments_parser" {
   function_name = "${var.env}-te-run-judgments-parser"
   role = aws_iam_role.retrieve_bagit_lambda_role.arn
   timeout = 30
+  environment {
+    variables = {
+      "S3_PARSER_BUCKET" = "dev-te-judgment-out"
+      "API_ENDPOINT" = var.api_endpoint
+    }
+  }
 }
 
 resource "aws_lambda_function" "editorial_integration" {
@@ -44,6 +50,12 @@ resource "aws_lambda_function" "editorial_integration" {
     variables = {
       "TE_VERSION_JSON" = jsonencode({"int-te-version": "0.0.0", "text-parser-version": "v0.0", "lambda-functions-version": [ { "int-te-bagit-checksum-validation": "0.0.0" }, { "int-te-files-checksum-validation": "0.0.0" }, { "int-text-parser-version": "v0.0" } ]})
       "TE_PRESIGNED_URL_EXPIRY" = 300
+      "S3_BUCKET" = "dev-te-judgment-out"
+      "S3_OBJECT_ROOT" = "parsed/"
+      "S3_FILE_PAYLOAD"="judgment.docx"
+      "S3_FILE_PARSER_XML"="te-xml.xml"
+      "S3_FILE_PARSER_META"="te-meta.json"
+      "S3_FILE_BAGIT_INFO"="bagit-info.txt"
     }
   }
 }
