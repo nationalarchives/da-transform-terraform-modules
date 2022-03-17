7c4a5b175826fac610e1e5520cc629cbfd9d4518
rahmedzaizi <rahmed@zaizi.com>
2022-03-17 14:28:42 +0000

    parser lambda added with apigw trigger

--- parser/apigateway.tf
+++ parser/apigateway.tf
@@ -5,7 +5,7 @@ resource "aws_apigatewayv2_api" "judgment_parser_api" {
 
 resource "aws_apigatewayv2_stage" "judgment_parser_api" {
   api_id = aws_apigatewayv2_api.judgment_parser_api.id
-  name = "${var.env}--te-judgment-parser-lambda"
+  name = "${var.env}-te-judgment-parser-lambda"
   auto_deploy = true
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.judgment_parser_api_logs.arn
