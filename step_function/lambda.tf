9ccdde6693b315f7a69a782c73fe0b5e1a3126d0
rahmedzaizi <rahmed@zaizi.com>
2022-03-22 09:24:35 +0000

    variables changed for lambdas

--- step_function/lambda.tf
+++ step_function/lambda.tf
@@ -27,21 +27,21 @@ resource "aws_lambda_function" "bagit_files_checksum_function" {
 }
 
 resource "aws_lambda_function" "run_judgments_parser" {
-  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-text-parser-step-function:0.0.9"
+  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-text-parser-step-function:0.0.10"
   package_type = "Image"
   function_name = "${var.env}-te-run-judgments-parser"
   role = aws_iam_role.retrieve_bagit_lambda_role.arn
   timeout = 30
   environment {
     variables = {
-      "S3_PARSER_BUCKET" = "dev-te-judgment-out"
+      "S3_PARSER_BUCKET" = aws_s3_bucket.editorial_judgment_out.bucket
       "API_ENDPOINT" = var.api_endpoint
     }
   }
 }
 
 resource "aws_lambda_function" "editorial_integration" {
-  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-editorial-integration:0.0.8"
+  image_uri = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-editorial-integration:0.0.9"
   package_type = "Image"
   function_name = "${var.env}-te-editorial-integration"
   role = aws_iam_role.retrieve_bagit_lambda_role.arn
@@ -49,11 +49,9 @@ resource "aws_lambda_function" "editorial_integration" {
   environment {
     variables = {
       "TE_VERSION_JSON" = jsonencode({"int-te-version": "0.0.0", "text-parser-version": "v0.0", "lambda-functions-version": [ { "int-te-bagit-checksum-validation": "0.0.0" }, { "int-te-files-checksum-validation": "0.0.0" }, { "int-text-parser-version": "v0.0" } ]})
-      "TE_PRESIGNED_URL_EXPIRY" = 300
-      "S3_BUCKET" = "dev-te-judgment-out"
+      "TE_PRESIGNED_URL_EXPIRY" = 60
+      "S3_BUCKET" = aws_s3_bucket.editorial_judgment_out.bucket
       "S3_OBJECT_ROOT" = "parsed/"
-      "S3_FILE_PAYLOAD"="judgment.docx"
-      "S3_FILE_PARSER_XML"="te-xml.xml"
       "S3_FILE_PARSER_META"="te-meta.json"
       "S3_FILE_BAGIT_INFO"="bagit-info.txt"
     }
