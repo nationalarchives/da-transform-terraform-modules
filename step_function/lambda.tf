44add6894950e9a96f12b39f83d4ba23e1794172
rahmedzaizi <rahmed@zaizi.com>
2022-03-30 11:03:27 +0100

    bagit checksum function update

--- step_function/lambda.tf
+++ step_function/lambda.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "retrieve_bagit_function" {
 }
 
 resource "aws_lambda_function" "bagit_files_checksum_function" {
-  image_uri     = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-files-checksum-validation:0.0.4"
+  image_uri     = "882876621099.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/te-files-checksum-validation:0.0.6"
   package_type  = "Image"
   function_name = "${var.env}-${var.prefix}-files-checksum-validation"
   role          = aws_iam_role.retrieve_bagit_lambda_role.arn
