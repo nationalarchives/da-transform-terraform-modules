532ab5026301088ac29442c192e10e636c0633cb
rahmedzaizi <rahmed@zaizi.com>
2022-04-12 17:17:36 +0100

    parser updates added to step function

--- step_function/iam.tf
+++ step_function/iam.tf
@@ -114,7 +114,8 @@ data "aws_iam_policy_document" "state_fucntion_lambda_policy" {
     resources = [
       aws_lambda_function.retrieve_bagit_function.arn,
       aws_lambda_function.bagit_files_checksum_function.arn,
-      aws_lambda_function.run_judgments_parser.arn,
+      aws_lambda_function.judgment_parser_lambda.arn,
+      aws_lambda_function.prepare_parser_input.arn,
       aws_lambda_function.editorial_integration.arn
     ]
   }
