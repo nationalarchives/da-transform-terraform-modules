a4e2d560701ba570ad114944fdc776588abd54f3
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-08-16 17:13:34 +0100

    Message structure changes

--- step_functions/receive_and_process_bag/lambda.tf
+++ step_functions/receive_and_process_bag/lambda.tf
@@ -55,7 +55,7 @@ resource "aws_lambda_function" "rapb_files_checksum_validation" {
 # rapb_step_function_trigger
 
 resource "aws_lambda_function" "rapb_trigger" {
-  image_uri = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-sqs-sf-trigger:${var.rapb_image_versions.tre_rapb_trigger}"
+  image_uri = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-rapb-trigger:${var.rapb_image_versions.tre_rapb_trigger}"
   package_type = "Image"
   function_name = local.lambda_name_trigger
   role = aws_iam_role.rapb_trigger_lambda.arn
