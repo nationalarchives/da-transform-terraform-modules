3ff6ea66cc79f202bdb690de32e74c3a042eca6c
rahmedzaizi <rahmed@zaizi.com>
2022-08-17 14:58:47 +0100

    added permisiion for tre-data-bucket

--- step_functions/dri_preingest_sip_generation/output.tf
+++ step_functions/dri_preingest_sip_generation/output.tf
@@ -2,3 +2,8 @@ output "dpsg_in_queue_arn" {
   value = aws_sqs_queue.tre_dpsg_in.arn
   description = "ARN of the TRE-DPSG SQS Queue"
 }
+
+output "dri_preingest_sip_generation_lambda_role" {
+  value = aws_iam_role.dri_preingest_sip_generation_lambda_role.arn
+  description = "ARN of the dpsg Lamda Role"
+}
