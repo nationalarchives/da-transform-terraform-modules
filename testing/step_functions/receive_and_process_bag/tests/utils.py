c3b277ff0d653082e0bf6123c5e28363fcb76c10
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-08-17 08:23:43 +0100

    Test via SNS topic

---
+++ testing/step_functions/receive_and_process_bag/tests/utils.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import time
+import uuid
+
+MESSAGE_VERSION = '1.0.0'
+
+def create_tdr_message(
+        environment: str,
+        consignment_type: str,
+        consignment_ref: str,
+        s3_bagit_url: str,
+        s3_sha_url: str,
+        message_version: str=MESSAGE_VERSION,
+        timestamp: int=int(time.time_ns()),
+        uuid_list: list=None
+) -> dict:
+    if uuid_list is None:
+        uuid_list = [
+            {
+                'TDR-UUID': str(uuid.uuid4())
+            }
+        ]
+
+    return {
+        'version': message_version,
+        'timestamp': timestamp,
+        'UUIDs': uuid_list,
+        'producer': {
+            'environment': environment,
+            'name': 'TDR',
+            'process': 'consignment-export test payload',
+            'type': consignment_type
+        },
+        'parameters': {
+            'TDR': {
+                'resource': {
+                    'resource-type': 'Object',
+                    'access-type': 'url',
+                    'value': s3_bagit_url
+                },
+                'resource-validation': {
+                    'resource-type': 'Object',
+                    'access-type': 'url',
+                    'validation-method': 'SHA256',
+                    'value': s3_sha_url
+                },
+                'number-of-retries': 0,
+                'reference': consignment_ref
+            }
+        }
+    }
