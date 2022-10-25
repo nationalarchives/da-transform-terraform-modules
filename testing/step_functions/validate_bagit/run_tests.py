5e87b4f69d125e3eb7b18e1c2091c45fcae83e98
Alex Ford <alex.ford@nationalarchives.gov.uk>
2022-10-25 14:14:54 +0100

    Changes placed on new branch to avoid additional files

--- testing/step_functions/validate_bagit/run_tests.py
+++ testing/step_functions/validate_bagit/run_tests.py
@@ -3,6 +3,7 @@ import logging
 import argparse
 from aws_test_lib.aws_tester import AWSTester
 from tests.test_ok_path import test_ok_path
+from tests.test_bad_paths import test_bad_consignment_type, test_bad_checksum
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,21 +18,53 @@ def main(
         aws_profile_management: str,
         aws_profile_deployment: str,
         environment_name: str,
-        test_consignment_ref: str
+        judgment_consignment_ref: str,
+        standard_consignment_ref: str
 ):
     logger.info(f'main: aws_profile_management={aws_profile_management} '
-        f'aws_profile_deployment={aws_profile_deployment} '
-        f'environment_name={environment_name}'
-        f'test_consignment_ref={test_consignment_ref}')
+                f'aws_profile_deployment={aws_profile_deployment} '
+                f'environment_name={environment_name}'
+                f'standard_consignment_ref={standard_consignment_ref}'
+                f'judgement_consignment_ref={judgment_consignment_ref}')
 
     at_management = AWSTester(aws_profile=aws_profile_management)
     at_deployment = AWSTester(aws_profile=aws_profile_deployment)
     s3_test_data_bucket = 'dev-te-testdata'  # management account
     tre_in_topic = f'{environment_name}-tre-in'  # deployment account
-    s3_output_bucket=f'{environment_name}-tre-common-data'  # deployment account
+    s3_output_bucket = f'{environment_name}-tre-common-data'  # deployment account
 
-    logger.info('*** testing judgment *********' + ('*' * 50))
-    consignment_type='judgment'
+    logger.info('*' * 130)
+    logger.info(('*' * 50) + ' testing bad checksum for file ' + ('*' * 50))
+    logger.info('*' * 130)
+
+    test_bad_checksum(
+        at_management=at_management,
+        at_deployment=at_deployment,
+        env=environment_name,
+        s3_test_data_bucket=s3_test_data_bucket,
+        s3_output_bucket=s3_output_bucket,
+        sns_input_topic=tre_in_topic
+    )
+
+    logger.info('*' * 130)
+    logger.info(('*' * 50) + ' testing bad consignment type' + ('*' * 50))
+    logger.info('*' * 130)
+
+    test_bad_consignment_type(
+        at_management=at_management,
+        at_deployment=at_deployment,
+        env=environment_name,
+        s3_test_data_bucket=s3_test_data_bucket,
+        s3_output_bucket=s3_output_bucket,
+        consignment_ref=judgment_consignment_ref,
+        sns_input_topic=tre_in_topic
+    )
+
+
+    logger.info('*' * 130)
+    logger.info(('*' * 50) + ' testing judgment consignment ' + ('*' * 50))
+    logger.info('*' * 130)
+    consignment_type = 'judgment'
 
     test_ok_path(
         at_management=at_management,
@@ -40,16 +73,19 @@ def main(
         s3_test_data_bucket=s3_test_data_bucket,
         s3_output_bucket=s3_output_bucket,
         consignment_type=consignment_type,
-        consignment_ref=test_consignment_ref,
+        consignment_ref=judgment_consignment_ref,
         sns_input_topic=tre_in_topic
     )
 
     logger.info(f'To see output, run: aws --profile {aws_profile_deployment} '
-            f's3 ls {s3_output_bucket}/consignments/{consignment_type}'
-            f'/{test_consignment_ref}/0/{test_consignment_ref}/')
+                f's3 ls {s3_output_bucket}/consignments/{consignment_type}'
+                f'/{judgment_consignment_ref}/0/{judgment_consignment_ref}/')
 
-    logger.info('*** testing standard *********' + ('*' * 50))
-    consignment_type='standard'
+    logger.info('*' * 130)
+    logger.info(('*' * 50) + 'testing standard consignment' + ('*' * 50))
+    logger.info('*' * 130)
+
+    consignment_type = 'standard'
 
     test_ok_path(
         at_management=at_management,
@@ -58,13 +94,13 @@ def main(
         s3_test_data_bucket=s3_test_data_bucket,
         s3_output_bucket=s3_output_bucket,
         consignment_type=consignment_type,
-        consignment_ref=test_consignment_ref,
+        consignment_ref=standard_consignment_ref,
         sns_input_topic=tre_in_topic
     )
 
     logger.info(f'To see output, run: aws --profile {aws_profile_deployment} '
-            f's3 ls {s3_output_bucket}/consignments/{consignment_type}'
-            f'/{test_consignment_ref}/0/{test_consignment_ref}/')
+                f's3 ls {s3_output_bucket}/consignments/{consignment_type}'
+                f'/{standard_consignment_ref}/0/{standard_consignment_ref}/')
 
     logger.info('###########################################################')
     logger.info('#                 All tests completed OK                  #')
@@ -78,13 +114,15 @@ if __name__ == "__main__":
         ))
 
     parser.add_argument('--aws_profile_management', type=str,
-            help='AWS_PROFILE name for management account')
+                        help='AWS_PROFILE name for management account')
     parser.add_argument('--aws_profile_deployment', type=str,
-            help='AWS_PROFILE name for deployment account')
+                        help='AWS_PROFILE name for deployment account')
     parser.add_argument('--environment_name', type=str,
-            help='Name of environment to test; e.g. dev, test, int, ...')
-    parser.add_argument('--test_consignment_ref', type=str,
-            help='The consignment reference to use for the tests')
+                        help='Name of environment to test; e.g. dev, test, int, ...')
+    parser.add_argument('--judgment_consignment_ref', type=str,
+                        help='The judgment consignment reference to use for the tests')
+    parser.add_argument('--standard_consignment_ref', type=str,
+                        help='The standard consignment reference to use for the tests')
 
     args = parser.parse_args()
 
@@ -92,5 +130,6 @@ if __name__ == "__main__":
         aws_profile_management=args.aws_profile_management,
         aws_profile_deployment=args.aws_profile_deployment,
         environment_name=args.environment_name,
-        test_consignment_ref=args.test_consignment_ref
+        judgment_consignment_ref=args.judgment_consignment_ref,
+        standard_consignment_ref=args.standard_consignment_ref
     )
--- testing/step_functions/validate_bagit/tests/test_ok_path.py
+++ testing/step_functions/validate_bagit/tests/test_ok_path.py
@@ -15,6 +15,7 @@ logging.basicConfig(
 # Instantiate logger
 logger = logging.getLogger(__name__)
 
+
 def test_ok_path(
         at_management: AWSTester,
         at_deployment: AWSTester,
@@ -23,32 +24,32 @@ def test_ok_path(
         s3_output_bucket: str,
         consignment_type: str,
         consignment_ref: str,
-        sns_input_topic: str=None  # run step function directly if set to None
+        sns_input_topic: str = None  # run step function directly if set to None
 ):
     logger.info(f'test_ok_path start: env={env} '
-        f'consignment_ref={consignment_ref} '
-        f'consignment_type={consignment_type}')
+                f'consignment_ref={consignment_ref} '
+                f'consignment_type={consignment_type}')
 
     # Remove any data from prior run of the consignment ref
     s3_delete_prefix = f'consignments/{consignment_type}/{consignment_ref}/'
     at_deployment.delete_from_s3(
-            s3_bucket_name=s3_output_bucket,
-            s3_object_prefix=s3_delete_prefix)
+        s3_bucket_name=s3_output_bucket,
+        s3_object_prefix=s3_delete_prefix)
 
     s3_bagit_url = at_management.get_presigned_url(
-            bucket=s3_test_data_bucket,
-            key=f'consignments/{consignment_type}/{consignment_ref}.tar.gz')
+        bucket=s3_test_data_bucket,
+        key=f'consignments/{consignment_type}/{consignment_ref}.tar.gz')
 
     s3_sha_url = at_management.get_presigned_url(
-            bucket=s3_test_data_bucket,
-            key=f'consignments/{consignment_type}/{consignment_ref}.tar.gz.sha256')
+        bucket=s3_test_data_bucket,
+        key=f'consignments/{consignment_type}/{consignment_ref}.tar.gz.sha256')
 
     input_dict = utils.create_tdr_message(
-            environment=env, 
-            consignment_type=consignment_type,
-            consignment_ref=consignment_ref,
-            s3_bagit_url=s3_bagit_url,
-            s3_sha_url=s3_sha_url)
+        environment=env,
+        consignment_type=consignment_type,
+        consignment_ref=consignment_ref,
+        s3_bagit_url=s3_bagit_url,
+        s3_sha_url=s3_sha_url)
 
     step_function_name = f'{env}-tre-validate-bagit'
     start_dtm = datetime.now(tz=timezone.utc)
@@ -66,7 +67,7 @@ def test_ok_path(
 
         logger.info(f'sns_publish_result={sns_publish_result}')
 
-    execution_detail_key_path='input.parameters.consignment-export.reference'
+    execution_detail_key_path = 'input.parameters.bagit-available.reference'
     step_function_executions = at_deployment.get_step_function_executions(
         step_function_name=step_function_name,
         from_date=start_dtm,
@@ -76,15 +77,15 @@ def test_ok_path(
 
     if len(step_function_executions) == 0:
         raise ValueError(f'Failed to find step function execution for '
-                f'{step_function_name} with execution value '
-                f'"{consignment_ref}" at key {execution_detail_key_path}')
+                         f'{step_function_name} with execution value '
+                         f'"{consignment_ref}" at key {execution_detail_key_path}')
 
     if step_function_executions[0]['status'] != 'SUCCEEDED':
         raise ValueError(f'Unexpected status "{step_function_executions[0]["status"]}"')
 
     step_result = at_deployment.get_step_function_step_result(
-          arn=step_function_executions[0]['executionArn'],
-          step_name='Files Checksum Validation')
+        arn=step_function_executions[0]['executionArn'],
+        step_name='Files Checksum Validation')
 
     logger.info(f'type(step_result)={type(step_result)}')
     logger.info(f'step_result={step_result}')
@@ -94,24 +95,25 @@ def test_ok_path(
     assert 'timestamp' in output, 'Missing timestamp'
     assert 'UUIDs' in output, 'Missing UUIDs'
     assert 'parameters' in output, 'Missing parameters'
-    
+
     parameters_tre = output['parameters']['bagit-validated']
-    assert len(parameters_tre['errors']) == 0, 'Error count > 0'
+    assert parameters_tre.get("errors") is None, 'Error count > 0'
     assert parameters_tre['s3-bucket'] == s3_output_bucket, 'Invalid s3-bucket value'
-    assert 's3-object-root' in parameters_tre, f's3-object-root key is missing'
-    assert 's3-bagit-name' in parameters_tre, f's3-bagit-name key is missing'
+    assert 's3-object-root' in parameters_tre, 's3-object-root key is missing'
+    assert 's3-bagit-name' in parameters_tre, 's3-bagit-name key is missing'
 
     sns_step_result = at_deployment.get_step_function_step_result(
-          arn=step_function_executions[0]['executionArn'],
-          step_name='SNS Publish')
-    
+        arn=step_function_executions[0]['executionArn'],
+        step_name='SNS tre-internal')
+
     logger.info(f'sns_step_result={sns_step_result}')
 
-    end_step_result = at_deployment.get_step_function_step_result(
-          arn=step_function_executions[0]['executionArn'],
-          step_name='Slack Alert Completed Successfully')
+    step_function_response = step_function_executions[0]['status']
 
-    logger.info(f'end_step_result={end_step_result}')
-    http_status_code = end_step_result['output']['SdkHttpMetadata']['HttpStatusCode']
-    assert http_status_code == 200, f'Expected HTTP status code 200 but got "{http_status_code}"'
-    logger.info('test_ok_path completed OK')
+    logger.info(f'end_step_result status code={step_function_response}')
+    assert step_function_response == "SUCCEEDED", f'Expected SUCCEEDED "{step_function_response}"'
+    logger.info('')
+
+    logger.info('=' * 150)
+    logger.info(('=' * 48) + f" test path completed OK for {consignment_type} consignment type " + ('=' * 48))
+    logger.info('=' * 150)
